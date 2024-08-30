96550b4a3e83e11939c3cce03d76f214cf8ff998
Vipul Raotole <raotole.vipul@gmail.com>
2024-08-30 23:24:43 +0100

    commit-1

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ class App extends Component {
 
   render() {
     const { auth } = this.props;
-    //if (auth.loading) return null;
-
     const { user = { name: "vipul" }, login, logout } = auth;
     const { editing } = this.state;
 
